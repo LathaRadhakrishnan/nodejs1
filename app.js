55909edf948848a0ac50e3bc848e97ecf96d3049
LathaRadhakrishnan <latharadhakrishnan17@gmail.com>
2024-02-07 11:58:18 +0530

    env testing in jenkins file

---
+++ app.js
@@ -0,0 +1,5 @@
+// Accessing environment variable in Node.js
+const myVariable = process.env.MY_VARIABLE;
+
+console.log(`Value of MY_VARIABLE: ${myVariable}`);
+// Your Node.js code continues here...
